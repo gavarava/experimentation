f144b56f7c9c11e1f1da125c28e4759e712d8884
Gaurav Edekar <gaurav.edekar@axiomatics.com>
2019-10-22 22:13:00 +0200

    Keyword Ranker Test

--- src/test/java/com/fun/keywordranker/KeywordRankerTest.java
+++ src/test/java/com/fun/keywordranker/KeywordRankerTest.java
@@ -7,9 +7,21 @@ import com.InputFetcher;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-;
-
 class KeywordRankerTest {
+
+    @Test
+    void shouldCalculateFrequencyOfWordsUsingList() {
+        InputFetcher inputFetcher = new InputFetcher("delimited-data-file-1megabyte", ",");
+        List<String> inputAsList = inputFetcher.getInputDataAsListOfStrings();
+        KeywordRanker keywordRanker = new KeywordRanker(inputAsList);
+
+        long startTime = System.currentTimeMillis();
+        long frequencyOfFirstWord = keywordRanker.calculateFrequencyOfWordUsingList(inputAsList.get(0));
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("shouldCalculateFrequencyOfWordsUsingList: Running time in milliseconds == > " + (endTime-startTime));
+        assertThat(frequencyOfFirstWord, is(4425L));
+    }
 
     @Test
     void shouldCalculateFrequencyOfWordsUsingArray() {
@@ -23,20 +35,6 @@ class KeywordRankerTest {
         long endTime = System.currentTimeMillis();
 
         System.out.println("shouldCalculateFrequencyOfWordsUsingArray: Running time in milliseconds == > " + (endTime-startTime));
-        assertThat(frequencyOfFirstWord, is(4425L));
-    }
-
-    @Test
-    void shouldCalculateFrequencyOfWordsUsingList() {
-        InputFetcher inputFetcher = new InputFetcher("delimited-data-file-1megabyte", ",");
-        List<String> inputAsList = inputFetcher.getInputDataAsListOfStrings();
-        KeywordRanker keywordRanker = new KeywordRanker(inputAsList);
-
-        long startTime = System.currentTimeMillis();
-        long frequencyOfFirstWord = keywordRanker.calculateFrequencyOfWordUsingList(inputAsList.get(0));
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("shouldCalculateFrequencyOfWordsUsingList: Running time in milliseconds == > " + (endTime-startTime));
         assertThat(frequencyOfFirstWord, is(4425L));
     }
 }
