c6aac566773f88346ec13f73e3e2c3ede7ef07ab
Gaurav Edekar <edekar.gaurav@gmail.com>
2020-01-12 13:18:22 +0100

    Refactor Code to Traverse an AcyclicGraph using DepthFirstSearch

--- src/main/java/com/algorithms/graphs/DepthFirstSearch.java
+++ src/main/java/com/algorithms/graphs/DepthFirstSearch.java
@@ -5,20 +5,24 @@ public class DepthFirstSearch {
     public static int findLargestValueInGraph(AcyclicGraph acyclicGraph) {
         int biggestElement = 0;
         int rootValue = acyclicGraph.getNodeValue();
-
         biggestElement = getBiggest(biggestElement, rootValue);
+        biggestElement = traverseGraphToGetBiggestElement(acyclicGraph, biggestElement);
+        return biggestElement;
+    }
 
+    private static int traverseGraphToGetBiggestElement(AcyclicGraph acyclicGraph, int latestBiggestElement) {
         for (AcyclicGraph node : acyclicGraph.getConnectedNodes()) {
             int nodeValue = node.getNodeValue();
+            latestBiggestElement = getBiggest(latestBiggestElement, nodeValue);
+            latestBiggestElement = getBiggestElementFromRestOfTheGraph(latestBiggestElement, node);
+        }
+        return latestBiggestElement;
+    }
 
-            if (nodeValue > biggestElement) {
-                biggestElement = nodeValue;
-            }
-
-            if (!node.isLeafNode()) {
-                int biggestFromChildren = findLargestValueInGraph(node);
-                biggestElement = getBiggest(biggestElement, biggestFromChildren);
-            }
+    private static int getBiggestElementFromRestOfTheGraph(int biggestElement, AcyclicGraph node) {
+        if (!node.isLeafNode()) {
+            int biggestFromChildren = findLargestValueInGraph(node);
+            biggestElement = getBiggest(biggestElement, biggestFromChildren);
         }
         return biggestElement;
     }
